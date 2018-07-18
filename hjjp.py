261db092d9cd894e580868502d1a5f5b98edd299
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-07-18 05:51:46 -0500

    Add files via upload

--- hjjp.py
+++ hjjp.py
@@ -6,16 +6,27 @@ try:
  url = 'https://dict.hjenglish.com/jp/jc/' + str(sys.argv[1])#终端参数
  r = requests.get(url,headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'})
  tree = lxml.html.fromstring(r.text)
- divsimple = tree.cssselect('header.word-details-pane-header')[0]
- word_temp = divsimple.text_content()
- for word in word_temp.split():
-  print (word)
+ a=[]
+ b=[]
+ c=[]
+ i=0
+ for div_temp1 in tree.cssselect('header.word-details-pane-header'):
+  word_temp1 = div_temp1.text_content()#提取翻译
+  a.append(word_temp1)
+  i=i+1
  
- print('\n详细解释/例句:')
- divsimple = tree.cssselect('div.word-details-item-content')[0]
- word_temp2 = divsimple.text_content()
- for word2 in word_temp2.split():
-  print(word2)
+ for div_temp2 in tree.cssselect('div.word-details-item-content'):
+  word_temp2 = div_temp2.text_content()#提取例句
+  b.append(word_temp2)
+ 
+ for n in range(i):
+  c.append(a[n]+b[n])#合并翻译与例句
+ 
+ for word_temp3 in c:
+  print('***分割线***')
+  for word in word_temp3.split():
+   print(word)
+
 
 except IndexError as e:
  print('抱歉，没有找到你查的单词结果、请核对拼写是否有误')
