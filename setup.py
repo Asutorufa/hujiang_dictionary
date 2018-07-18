89265b170e7d5773322878249392bd7b22232595
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-07-18 09:19:32 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "hjjpcj",
+    version = "0.8.1",
+    keywords = ("japanese dict dictionary hujiang"),
+    description = "hujiang dictionary",
+    long_description = "a dictionary from hujiang to search japanese word",
+    license = "MIT Licence",
+
+    url = "https://github.com/Asutorufa/hujiang-japanese-dict",
+    author = "Asutorufa",
+    author_email = "pip@just2hide.anyalias.com",
+    entry_points={'console_scripts':['hjjp = hjjp.hjjp:main']},
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
