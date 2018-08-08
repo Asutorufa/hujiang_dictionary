97046adca02b7f30aa9e7805556182728e8c4bd2
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2018-08-08 13:36:58 +0800

    0.9.8

--- hjjp/hjjp.py
+++ hjjp/hjjp.py
@@ -10,35 +10,36 @@ css_word_prnounces  = 'div.word-info > div.pronounces'
 css_word_prnounces_kata = 'span'
 css_word_info='div.simple'
 css_word_example='div.word-details-item-content'
-voice_switch = 0
 language=['jp','jc']
-useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36'
 
 def option():
     parser = argparse.ArgumentParser()
-    parser.add_argument('word',type=str,help='need search word',nargs='*')
-    parser.add_argument("-v","--voice",help="voice link",action="store_true")
+    parser.add_argument('word',type=str,help='word',nargs='*')
+    parser.add_argument("-v","--voice",help="link of voice",action="store_true")
     args = parser.parse_args()
     return args.word,args.voice
 
 def start(args_word,voice_switch):
     for word in args_word:
         n=0
-        cprint('\n查询单词为  '+word+'\n','red',attrs=['bold'])
+        cprint('----------查询单词 '+word+'---------','red',attrs=['bold'])
         url = 'https://dict.hjenglish.com/'+language[0]+'/'+language[1]+'/' + word
         tree = lxml.html.fromstring((requests.get(url,headers={'User-Agent':useragent})).text)
         word_prnounces = word_simple(tree)
         word_voice = voice(tree)
         word_example = word_example_f(tree)
+        if word_prnounces == []:
+            cprint('抱歉，没有找到你查的单词结果,请核对拼写是否有误\n','cyan')
         for word_info_temp in tree.cssselect(css_word_info):
-            print('\n结果'+str(n+1))
+            print('\n '+word+' 查询结果 '+str(n+1))
             cprint(word_prnounces[n],'yellow')
-            if voice_switch==1:
-                cprint('音频链接:   '+word_voice[n],'blue',attrs=['underline','bold'])
+            if voice_switch:
+                cprint('读音链接:   '+word_voice[n],'blue',attrs=['underline','bold'])
             for sub_word_info in word_info_temp.text_content().split():
                 cprint(sub_word_info,'cyan')
             
-            cprint('\n'+'详细解释:','magenta',attrs=['bold'])
+            cprint('详细解释:','magenta',attrs=['bold'])
             for sub_word_example in word_example[n]:
                 cprint(sub_word_example,'green')
 
@@ -73,7 +74,7 @@ def word_example_f(tree):
 
 
 def main():
-    tree = start(option()[0],option()[1])
+    start(option()[0],option()[1])
 
 if __name__ == '__main__':
     main()
